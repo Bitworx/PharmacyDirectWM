176150bc51b38a7d2240ea597e8c9a130a62fb9b
Bitworx <Bitworx@mweb.co.za>
2017-04-23 14:20:35 +0200

    test

---
+++ clsCounters.vb
@@ -0,0 +1,43 @@
+﻿Public Class ManifestCounters
+
+    Private manifestLoaded As Integer = 0
+    Private manifestDelivered As Integer = 0
+    Private manifestToUpload As Integer = 0
+
+    Property LoadedManifests() As Integer
+        Get
+            Return manifestLoaded
+        End Get
+        Set(ByVal value As Integer)
+            manifestLoaded = value
+        End Set
+    End Property
+
+    Property DeliveredManifests()
+        Get
+            Return manifestDelivered
+        End Get
+        Set(ByVal value)
+            manifestDelivered = value
+        End Set
+    End Property
+
+    Property UploadManifest()
+        Get
+            Return manifestToUpload
+        End Get
+        Set(ByVal value)
+            manifestToUpload = value
+        End Set
+    End Property
+
+    Sub New()
+        manifestLoaded = 0
+        'Check in local db for loaded Manifests
+
+        'Check in local db for manifests to upload to web
+
+        'Check in local db for manifests delivered
+
+    End Sub
+End Class
---
+++ clsDriver.vb
@@ -0,0 +1,27 @@
+﻿Public Class Driver
+
+    Private driverName As String = ""
+    Private driverID As String = ""
+
+    Property ID() As String
+        Get
+            Return driverID
+        End Get
+        Set(ByVal value As String)
+            driverID = value
+        End Set
+    End Property
+
+    Property Name() As String
+        Get
+            Return driverName
+        End Get
+        Set(ByVal value As String)
+            driverName = value
+        End Set
+    End Property
+
+    Sub New()
+        driverName = ""
+    End Sub
+End Class
